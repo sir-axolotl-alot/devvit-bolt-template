cf337815ba1f289b66dac39168ce4bc696055d18
Edgar Neto <edgar.neto@reddit.com>
2025-04-01 20:21:36 +0100

    Improved prompt, initialized with mocked responses by default

--- web-view-app/src/App.tsx
+++ web-view-app/src/App.tsx
@@ -3,7 +3,9 @@ import devvitClient from './lib/DevvitClient';
 
 function App() {
   useEffect(() => {
-    const useMockedResponses = false;
+    // Set to true to use mocked responses on Bolt
+    // Set to false when running `devvit playtest` to run the app with the real backend on Reddit
+    const useMockedResponses = true; 
     devvitClient.initialize(useMockedResponses);
   }, []);
 
