0d064f448c90409f4e7890bae70aa9e16e21a440
Edgar Neto <edgar.neto@reddit.com>
2025-04-01 21:20:23 +0100

    Reverting back to mocked responses

--- web-view-app/src/App.tsx
+++ web-view-app/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     // Set to true to use mocked responses on Bolt
     // Set to false when running `devvit playtest` to run the app with the real backend on Reddit
-    const useMockedResponses = false; 
+    const useMockedResponses = true; 
     devvitClient.initialize(useMockedResponses);
 
 
